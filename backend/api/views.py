ab0a11b1bf748628281fc32792228449455c643b
Sanjay Sunar <sansunar11@gmail.com>
2025-01-14 20:12:58 +0545

    login and registation serializer and view
    - MyTokenObtainPairSerializer Class to generate and refresh Token
    - Registration Serializer Class to register users
    - token and registration vies in Urls.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from api import serializers as api_serializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+from userauths.models import User, Profile
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = api_serializer.MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.RegisterSerializer
